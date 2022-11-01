5beae68da1a1daab7f5de9078b8050f0bc9ebf30
date2251 <biz76216@gmail.com>
2022-11-01 14:47:28 +0900

    RankingPage.vue タブ→スペース

--- front/src/RankingPage.vue
+++ front/src/RankingPage.vue
@@ -1,19 +1,19 @@
 <template>
-	<div>
-		<navbar></navbar>
-		<div class="content">
-			<table>
-				<tr>
-					<th>ユーザー</th><th>試行回数</th>
-				</tr>
-				<tr v-for="rank in rankings" :key="rank.id">
-					<td>{{ rank.username }}</td><td>{{ rank.trynum }}</td>
-				</tr>
-			</table>
-			<sidebar></sidebar>
-		</div>
-		<footerarea></footerarea>
-	</div>
+  <div>
+    <navbar></navbar>
+    <div class="content">
+      <table>
+        <tr>
+          <th>ユーザー</th><th>試行回数</th>
+        </tr>
+        <tr v-for="rank in rankings" :key="rank.id">
+          <td>{{ rank.username }}</td><td>{{ rank.trynum }}</td>
+        </tr>
+      </table>
+      <sidebar></sidebar>
+    </div>
+    <footerarea></footerarea>
+  </div>
 </template>
 
 <script>
@@ -22,36 +22,36 @@ import sidebar from './SideBar.vue'
 import footerarea from './FooterArea.vue'
 
 export default {
-	components: {
-		sidebar,
-		navbar,
-		footerarea
-	},
-	data () {
-		return {
-			rankings: []
-		}
-	},
-	mounted () {
-		this.$axios
-			.get('/api/v1/rankings')
-			.then(response => (this.rankings = response.data))
-	}
+  components: {
+    sidebar,
+    navbar,
+    footerarea
+  },
+  data () {
+    return {
+      rankings: []
+    }
+  },
+  mounted () {
+    this.$axios
+      .get('/api/v1/rankings')
+      .then(response => (this.rankings = response.data))
+  }
 }
 </script>
 
 <style scoped>
 .content {
-	display: flex;
-	justify-content: space-between;
-	padding: 0 5%;
+  display: flex;
+  justify-content: space-between;
+  padding: 0 5%;
 }
 
 table {
-	border-collapse: collapse;
+  border-collapse: collapse;
   border-spacing: 0;
   width: 100%;
-	text-align: left;
+  text-align: left;
 }
 
 table tr{
