f676d25b819ca9e379d28144e96c6658c3cd56bb
date2251 <biz76216@gmail.com>
2022-11-01 17:31:56 +0900

    spec/rails_helper.rb config.include RSpec::RequestDescriber, type: :request

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'rspec/request_describer'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
@@ -31,6 +32,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include RSpec::RequestDescriber, type: :request
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
